9289002bbc1c1e04fc17954592579e37a162baf5
Marslayman <490135048@qq.com>
2018-01-29 17:48:29 +0800

    RELEASE
    RELEASE 2.0

--- src/components/Signin/Signin.vue
+++ src/components/Signin/Signin.vue
@@ -16,18 +16,65 @@
       </x-input>
     </group>
     <br>
-    <x-button @click.native="register" class="sub-btn" plain> 注册 </x-button>
+    <x-button @click.native="compInfo" class="sub-btn" plain> 注册 </x-button>
     </div>
     <div class="link">
       <router-link to="/login">返回登录</router-link>
     </div>
+
+    <!-- sex -->
+    <div v-transfer-dom>
+      <popup v-model="step1" position="right" width="100%" style="background-color: white;">
+        <div class="sex">
+          <div class="center">
+            <group label-algin="justify">
+              <h3 slot="title">请选择你的性别</h3>
+              <radio :options="sexitem" v-model="sex">
+                <template slot-scope="props" slot="each-item"><!-- use scope="props" when vue < 2.5.0 -->
+                  <p>
+                    <i class="iconfont icon-sexm" v-if="props.label === '男'"></i>
+                    <i class="iconfont icon-sexw" v-if="props.label === '女'"></i>
+                    {{props.label}}
+                  </p>
+                </template>
+              </radio>
+            </group>
+          </div>
+          <div class="next">
+            <a href="#" @click="nextStep">下一项</a>
+          </div>
+        </div>
+      </popup>
+    </div>
+
+
+    <!-- age -->
+    <div v-transfer-dom>
+      <popup v-model="step2" position="right" width="100%" style="background-color: white;">
+        <div class="age">
+          <div class="center">
+            <group label-algin="justify">
+              <h3 slot="title">请设置你的生日</h3>
+              <datetime-view v-model="birthday" ref="datetime" :format="format" start-date="1960-01-01"></datetime-view>
+            </group>
+          </div>
+          <div class="next">
+            <a @click="preStep" class="pre">上一项</a>
+            <a @click="register">完成</a>
+          </div>
+        </div>
+      </popup>
+    </div>
+
   </div>
 </template>
 
 <script>
-import { XInput, Group, XButton } from 'vux'
+import { XInput, Group, XButton, Popup, TransferDom, Radio, DatetimeView } from 'vux'
 export default {
-  name: 'sigin',
+  directives: {
+    TransferDom
+  },
   data () {
     return {
       tel: '',
@@ -36,7 +83,13 @@ export default {
       buttonText: '发送验证码',
       avail: false,
       valcode: '',
-      count: 60
+      count: 60,
+      step1: false,
+      step2: false,
+      format: 'YYYY-MM-DD',
+      birthday: '2018-01-01',
+      sexitem: ['男', '女'],
+      sex: ''
     }
   },
   mounted () {
@@ -79,11 +132,49 @@ export default {
         }
       })
     },
-    register () {
-      var _this = this
+    preStep () {
+      this.step2 = false
+    },
+    nextStep () {
+      // this.step1 = false;
+      this.step2 = true
+    },
+    compInfo () {
       if (this.tel === '' || this.password === '' || this.confirm === '' || this.valcode === '') {
         return;
       }
+      this.step1 = true
+    },
+    register () {
+      var _this = this
+      var subinfo = {
+        'tel': this.tel,
+        'password': this.confirm,
+        'sex': this.sex,
+        'birthday': this.birthday
+      }
+      var current = new Date()
+      var date = current.toLocaleDateString()
+      var bdate = _this.birthday
+      bdate.replace(/-/g, '\/')
+      if (bdate > date) {
+        this.$vux.alert.show({
+          title: '错误提示',
+          content: '你确定你是来自未来吗？<br>请选择正确出生日期'
+        })
+        return
+      }
+      if (this.tel === '' || this.password === '' || this.confirm === '' || this.valcode === '') {
+        return;
+      }
+      if (this.sex === '') {
+        this.$vux.alert.show({
+          title: '错误提示',
+          content: '请选择您的性别'
+        })
+        return
+      }
+
       var telreg = /^1[3|4|5|7|8][0-9]{9}$/ // 正则表达式对用户名做规则匹配
       var pwdreg = /^[a-zA-Z0-9]{6,8}$/ // 正则表达式对密码做规则匹配
       if (!this.tel.match(telreg) || !this.password.match(pwdreg)) {
@@ -100,13 +191,13 @@ export default {
         })
         return
       }
-      this.$store.dispatch('getSigin', {'tel': this.tel, 'confirm': this.confirm}).then(res => {
+      this.$store.dispatch('getSigin', subinfo).then(res => {
         if (res.status === 'yes') {
           this.$vux.alert.show({
             title: '成功提示',
             content: '恭喜你，你已经注册成功！<br/>将跳转至登录页面',
             onHide () {
-              _this.$router.push('/login')
+              _this.$router.push('/index')
             }
           })     
         } else {
@@ -121,12 +212,63 @@ export default {
   components: {
     XInput,
     Group,
-    XButton
+    XButton,
+    Popup,
+    Radio,
+    DatetimeView
   }
 }
 </script>
 
 <style lang="scss" scoped>
+.sex, .age{
+  position: relative;
+  height: 100%;
+  .center {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translateX(-50%) translateY(-50%);
+    h3 {
+      color:lightslategray;
+    }
+    i {
+      font-size: 24px;
+      display: inline-block;
+      padding-right: 28px;
+    }
+    p {
+      font-size: 28px;
+      padding: 12px 0px;
+      color: lightslategray;
+      .icon-sexm {
+        color: blue;
+      }
+      .icon-sexw {
+        color: magenta;
+      }
+    }
+  }
+  .next {
+    position: absolute;
+    width: 100%;
+    bottom: 0;
+    left: 0;
+    padding: 32px;
+    font-size: 16px;
+    line-height: 1em;
+    text-align: right;
+    a {
+      text-decoration: none;
+      color:#88D9DA;
+      display: inline-block;
+      padding: 12px;
+      &.pre {
+        float: left;
+      }
+    }
+  }
+}
 #signin {
   position: relative;
   background-color: #88D9DA;
